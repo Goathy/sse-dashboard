2aacdf9c3f7ed634a619da16fb992805c981871e
Jakub Gałecki <goathy.dev@gmail.com>
2023-05-20 10:36:05 +0200

    stream multiple values

--- server.test.mjs
+++ server.test.mjs
@@ -2,6 +2,7 @@ import assert from 'node:assert'
 import { it } from 'node:test'
 import undici from 'undici'
 import { buildServer } from './server.mjs'
+import { setTimeout } from 'node:timers/promises'
 
 it('should pass', async (t) => {
   const fastify = buildServer()
@@ -76,4 +77,41 @@ it('check response using raw response', async (t) => {
 
   assert.equal(calls, 1, 'body should return only one value')
   assert.equal(responseBody, '{"Hello":"World"}')
+})
+
+it('stream multiple values over time', async (t) => {
+  const fastify = buildServer()
+
+  t.after(async () => {
+    await fastify.close()
+  })
+
+  await fastify.register(import('./requests.plugin.mjs'))
+
+  fastify.get('/test', async (_, reply) => {
+    reply.raw.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache'
+    })
+
+    for (const idx of [1, 2, 3]) {
+      await setTimeout(0)
+      reply.raw.write(`value: ${idx}\n`)
+    }
+
+    reply.raw.end()
+  })
+
+  await fastify.listen()
+
+  const { body } = await undici.request('http://localhost:' + fastify.server.address().port + '/test', { method: 'GET' })
+
+  const responseBody = []
+
+  for await (const data of body) {
+    responseBody.push(String(data))
+  }
+
+  assert.deepEqual(responseBody, ['value: 1\n', 'value: 2\n', 'value: 3\n'])
 })
