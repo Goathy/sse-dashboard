5ce6cb5b33213c1d788fe2884a78f186606c1bc1
Jakub Gałecki <goathy.dev@gmail.com>
2023-05-20 15:13:25 +0200

    close connection using Map reference

--- server.test.mjs
+++ server.test.mjs
@@ -3,6 +3,8 @@ import { it } from 'node:test'
 import undici from 'undici'
 import { buildServer } from './server.mjs'
 import { setTimeout } from 'node:timers/promises'
+import { setImmediate } from 'node:timers'
+import { EventEmitter } from 'node:events'
 
 it('should pass', async (t) => {
   const fastify = buildServer()
@@ -109,3 +111,143 @@ it('stream multiple values over time', async (t) => {
 
   assert.deepEqual(responseBody, ['value: 1\n', 'value: 2\n', 'value: 3\n'])
 })
+
+it('stream multiple values over time reusing response', async (t) => {
+  const fastify = buildServer()
+
+  t.after(async () => {
+    await fastify.close()
+  })
+
+  await fastify.register(import('./responses.plugin.mjs'))
+
+  fastify.get('/sse', (_, reply) => {
+    reply.raw.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache'
+    })
+
+    fastify.responses.set('1', reply.raw)
+  })
+
+  fastify.get('/send', async (_, reply) => {
+    for (const idx of [1, 2, 3]) {
+      await setTimeout(0)
+      fastify.responses.get('1').write(`value: ${idx}\n`)
+    }
+
+    fastify.responses.get('1').end()
+
+    reply.send()
+  })
+
+  await fastify.listen()
+
+  setImmediate(async () => await undici.request('http://localhost:' + fastify.server.address().port + '/send', { method: 'GET' }))
+
+  const { body } = await undici.request('http://localhost:' + fastify.server.address().port + '/sse', { method: 'GET' })
+
+  const responseBody = []
+
+  for await (const data of body) {
+    responseBody.push(String(data))
+  }
+
+  assert.deepEqual(responseBody, ['value: 1\n', 'value: 2\n', 'value: 3\n'])
+})
+
+it('closed response should remove reference in responses map', async (t) => {
+  const fastify = buildServer()
+
+  t.after(async () => {
+    await fastify.close()
+  })
+
+  await fastify.register(import('./responses.plugin.mjs'))
+
+  let isClosed = false
+
+  fastify.get('/sse', (_, reply) => {
+    reply.raw.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache'
+    })
+
+    fastify.responses.set('1', reply.raw)
+
+    reply.raw.on('close', () => {
+      fastify.responses.delete('1')
+      isClosed = true
+    })
+  })
+
+  fastify.get('/send', async (_, reply) => {
+    for (const idx of [1, 2, 3]) {
+      await setTimeout(0)
+      fastify.responses.get('1').write(`value: ${idx}\n`)
+    }
+
+    fastify.responses.get('1').end()
+
+    reply.send()
+  })
+
+  await fastify.listen()
+
+  setImmediate(async () => await undici.request('http://localhost:' + fastify.server.address().port + '/send', { method: 'GET' }))
+
+  const { body } = await undici.request('http://localhost:' + fastify.server.address().port + '/sse', { method: 'GET' })
+
+  const responseBody = []
+
+  for await (const data of body) {
+    responseBody.push(String(data))
+  }
+
+  assert.deepEqual(responseBody, ['value: 1\n', 'value: 2\n', 'value: 3\n'])
+  assert.equal(fastify.responses.size, 0, 'response should be deleted after close')
+  assert.ok(isClosed, 'response should be closed')
+}
+)
+
+it('close connection using Map reference', async (t) => {
+  const fastify = buildServer()
+
+  const requestId = 'requestId-1'
+
+  let calls = 0
+
+  const ee = new EventEmitter()
+
+  t.after(async () => {
+    await fastify.close()
+    ee.removeAllListeners()
+  })
+
+  await fastify.register(import('./responses.plugin.mjs'))
+
+  fastify.get('/sse', (_, reply) => {
+    reply.raw.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache'
+    })
+
+    fastify.responses.set(requestId, reply.raw)
+
+    ee.emit('called')
+
+    reply.raw.on('close', () => { fastify.responses.delete(requestId); calls++ })
+  })
+
+  await fastify.listen()
+
+  ee.once('called', () => { fastify.responses.get(requestId).end() })
+
+  const { body } = await undici.request('http://localhost:' + fastify.server.address().port + '/sse', { method: 'GET' })
+
+  body.on('close', () => assert.equal(calls, 1, 'connection should be closed'))
+}
+)
